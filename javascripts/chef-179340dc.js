3fba1b64d000157379fd8a63a6a6e3b0f2322c46
leopard.not.a@gmail.com <leopard.not.a@gmail.com>
2015-02-20 11:39:31 +0200

    Automated commit at 2015-02-20 09:39:31 UTC by middleman-deploy 1.0.0

---
+++ javascripts/chef-179340dc.js
@@ -0,0 +1,1 @@
+(function(){$(function(){return $("#socialSharingBlock").is(":visible")?(window.addthis_config={data_track_addressbar:!0},window.addthis_share={title:"Cooking Infrastructure by Chef",description:"Cooking Infrastructure by Chef, free book about DevOps"},$.getScript("http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-527012705413f0b7")):void 0})}).call(this);
